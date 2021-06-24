6861f94f063651f929040bc0e49446f08231306a
aniketbachhas <86190666+aniketbachhas@users.noreply.github.com>
2021-06-24 23:02:31 +0530

    Update treepersqkm.py

--- treepersqkm.py
+++ treepersqkm.py
@@ -1,3 +1,9 @@
+#Problem Statement:
+#treepersqkm is a dictionary showing the tree number of countries per square kilometer for random countries with sizeable population numbers. 
+#Write a function named "moretrees" that returns a list of country names with less than 20000 trees per square kilometer. 
+#treepersqkm = {"Finland":90652, "Taiwan": 69593, "Japan": 49894, "Russia": 41396, "Brazil": 39542, "India": 11109, "Denmark": 6129, "Syria": 534, "Saudi Arabia": 1}
+
+#Solution:
 def moretrees():
     for key, value in treepersqkm.items():
         if value<=20000:
@@ -7,5 +13,3 @@ treepersqkm = {"Finland": 90652, "Taiwan": 69593, "Japan": 49894, "Russia": 4139
 
 print("\n\nCountries with less than 20,000 trees per sq.km.:")
 moretrees()
-
-
